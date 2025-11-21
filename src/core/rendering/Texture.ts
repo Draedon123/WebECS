860e828ac5e87152c059bd9c6161cbc2190f30ab
Draedon <draedon123@gmail.com>
2025-11-21 22:14:09 +1000

    Texture class

---
+++ src/core/rendering/Texture.ts
@@ -0,0 +1,62 @@
+import { Component } from "src/ecs";
+
+class Texture extends Component {
+  public static readonly tag: string = "Texture";
+
+  public texture!: GPUTexture;
+
+  private readonly source: GPUCopyExternalImageSource;
+  private readonly width: number;
+  private readonly height: number;
+  private readonly label?: string;
+
+  constructor(
+    source: GPUCopyExternalImageSource,
+    width: number,
+    height: number,
+    label?: string
+  ) {
+    super(Texture.tag);
+
+    this.source = source;
+    this.width = width;
+    this.height = height;
+    this.label = label;
+  }
+
+  public get initialised(): boolean {
+    return this.texture !== undefined;
+  }
+
+  public initialise(device: GPUDevice): void {
+    this.texture = device.createTexture({
+      label: this.label,
+      size: [this.width, this.height],
+      format: "rgba8unorm",
+      usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST,
+    });
+
+    device.queue.copyExternalImageToTexture(
+      {
+        source: this.source,
+      },
+      {
+        texture: this.texture,
+        origin: [0, 0],
+      },
+      {
+        width: this.width,
+        height: this.height,
+      }
+    );
+  }
+
+  public static async fetch(url: string, label?: string): Promise<Texture> {
+    const data = await (await fetch(url)).blob();
+    const bitmap = await createImageBitmap(data);
+
+    return new Texture(bitmap, bitmap.width, bitmap.height, label);
+  }
+}
+
+export { Texture };
